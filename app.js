47a22295a00c8a6ac2446e83f8bc62c38931b6e8
Josh Petry <josh.petry@outlook.com>
2019-01-20 12:45:10 -0500

    Using the Event Argument
    'li' elements inside the listDiv turn uppercase/lowercase when hovered over

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const listItems = document.getElementsByTagName('li');
 
-listDiv.addEventListener('mouseover', () => {
-  listItems[i].textContent = listItems[i].textContent.toUpperCase();
+
+//make 'li' elevents inside the listDiv turn uppercase/lowercase when hover over
+listDiv.addEventListener('mouseover', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toUpperCase();
+    }
 });
-listDiv.addEventListener('mouseout', () => {
-  listItems[i].textContent = listItems[i].textContent.toLowerCase();
+listDiv.addEventListener('mouseout', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toLowerCase();
+    }
 });
+
+
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
